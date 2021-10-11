db381276b5034d40d5b8796807c70c510a72130c
Daniel Clarke <u8sand@gmail.com>
2021-10-11 16:37:43 -0400

    Add source code

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='deriva_datapackage',
+  version='1.0.0',
+  url='https://github.com/nih-cfde/',
+  author='Daniel J. B. Clarke',
+  author_email='danieljbclarkemssm@gmail.com',
+  long_description=open('README.md', 'r').read(),
+  license='Apache-2.0',
+  install_requires=list(map(str.strip, open('requirements.txt', 'r').readlines())),
+  packages=find_packages(),
+  include_package_data=True,
+)
