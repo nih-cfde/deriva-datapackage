f827724bc0ea417fb0f1e2df52240f941b211b5a
Daniel Clarke <u8sand@gmail.com>
2025-01-07 18:42:12 -0500

    1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='deriva-datapackage',
-  version='1.0.6',
+  version='1.1.0',
   url='https://github.com/nih-cfde/',
   author='Daniel J. B. Clarke',
   author_email='danieljbclarkemssm@gmail.com',
